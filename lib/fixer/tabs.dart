ae07c217e3e2e50237df11112df92840a3cb1dd7
weaming <garden.yuen@gmail.com>
2018-07-07 13:44:48 +0800

    add RatesForm

--- lib/fixer/tabs.dart
+++ lib/fixer/tabs.dart
@@ -73,52 +73,158 @@ class RateListState extends State<RateList> {
 }
 
 Widget renderRates(Map rates, String selectedCurrency, {double amount=100.0}) {
-  var keys = rates.keys
-      .where((x) => CURRENCY_NAMES[x].length > 0)
-      .map((x) => '$x').toList();
-  keys.sort();
+  return RatesForm(rates, selectedCurrency);
+}
 
-  var items = keys.map((k) => [k, rates[k]]).toList();
+class RatesForm extends StatefulWidget {
+  final rates;
+  final selectedCurrency;
 
-  return ListView.builder(
-    itemCount: keys.length,
-    itemBuilder: (context, index) {
-      var item = items[index];
-      var currency = item[0];
-      var rate = ((item[1] / rates[selectedCurrency]) * amount)
-          .toStringAsFixed(3);
+  RatesForm(this.rates, this.selectedCurrency);
 
-      var rv = Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          Text(
-            '$currency ${CURRENCY_NAMES[currency]}',
-            style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color:
-                currency == selectedCurrency
-                    ? Colors.deepOrange
-                    : Colors.blue),
-          ),
-          Chip(
-            label: Text(
-              '$rate',
+  @override
+  createState() => _RatesFormState(rates, selectedCurrency);
+}
+
+class _RatesFormState extends State<RatesForm> {
+  final rates;
+  var selectedCurrency;
+  var amount = 100.0;
+
+  _RatesFormState(this.rates, this.selectedCurrency);
+
+  @override
+  build(BuildContext context) {
+    var keys = rates.keys
+        .where((x) => CURRENCY_NAMES[x].length > 0)
+        .map((x) => '$x').toList();
+    keys.sort();
+
+    var items = keys.map((k) => [k, rates[k]]).toList();
+
+    final amountController = TextEditingController(
+        text: '$amount',
+    );
+
+    @override
+    void dispose() {
+      // Clean up the controller when the Widget is disposed
+      amountController.dispose();
+      super.dispose();
+    }
+
+    var _notified = false;
+    bool _parseTextToNum(BuildContext context, String text) {
+      if (text.length == 0) {
+        return true;
+      }
+
+      var parsed;
+      try {
+        parsed = double.parse(text);
+        _notified = false;
+      } catch (e) {
+        if (!_notified) {
+          _notified = true;
+          Scaffold.of(context).showSnackBar(
+            SnackBar(
+              content: Text("请输入数字"),
+              duration: Duration(seconds: 1),
+            ),
+          );
+        }
+        return false;
+      }
+
+      setState(() {
+        amount = parsed;
+      });
+      return true;
+    }
+
+    var alertInput = AlertDialog(
+      title: Text("输入数量"),
+      content: TextField(
+        autofocus: true,
+        controller: amountController,
+        onSubmitted: (text) {
+          if (_parseTextToNum(context, text)){
+            Navigator.pop(context);
+          }
+        },
+        onChanged: (text) {
+          _parseTextToNum(context, text);
+        },
+      ),
+    );
+
+    var listView = ListView.builder(
+      itemCount: keys.length,
+      itemBuilder: (context, index) {
+        var item = items[index];
+        var currency = item[0];
+        var rate = ((item[1] / rates[selectedCurrency]) * amount)
+            .toStringAsFixed(3);
+
+        // tap to switch base currency
+        var onTap;
+        if (currency != selectedCurrency) {
+          onTap = () {
+            setState(() {
+              selectedCurrency = currency;
+            });
+          };
+        } else {
+          onTap = () {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return alertInput;
+              }
+            );
+          };
+        }
+
+        var rv = Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text(
+              '$currency ${CURRENCY_NAMES[currency]}',
               style: TextStyle(
-                fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                  color:
+                  currency == selectedCurrency
+                      ? Colors.deepOrange
+                      : Colors.blue),
+            ),
+            Chip(
+              padding: EdgeInsets.all(10.0),
+              label: Text(
+                rate,
+                textAlign: TextAlign.end,
+                style: TextStyle(
+                  fontSize: 30.0,
+                ),
               ),
             ),
-          ),
-        ],
-      );
+          ],
+        );
 
-      return Card(
-        margin: EdgeInsets.symmetric(vertical: 2.5, horizontal: 5.0),
-        child: Container(
-          child: rv,
-          margin: EdgeInsets.all(10.0),
-        ),
-        color: Colors.white70,
-      );
-    },
-  );
+
+        return GestureDetector(
+          child: Card(
+            margin: EdgeInsets.symmetric(vertical: 2.5, horizontal: 5.0),
+            child: Container(
+              child: rv,
+              margin: EdgeInsets.all(10.0),
+            ),
+            color: Colors.white70,
+          ),
+          onDoubleTap: onTap,
+        );
+      },
+    );
+
+    return listView;
+  }
 }
